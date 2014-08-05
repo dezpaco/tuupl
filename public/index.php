6e8cbb3ac701ad4af61fd2f70e712082cf2d0600
Derry Lozano-Hoyland <derry@derryhoyland.com>
2014-08-05 12:58:36 +0100

    Renamed error pages to notfound

--- public/index.php
+++ public/index.php
@@ -15,7 +15,7 @@ $twig = new Twig_Environment($loader);
 
 $routes = array(
 	'/' => array('index', array('bodyId' => 'home')),
-    '/error' => array('error', array('bodyId' => 'notfound'))
+    '/notfound' => array('notfound', array('bodyId' => 'notfound'))
 );
 
 $view = $routes[$_SERVER['REQUEST_URI']];
--- public/notfound.php
+++ public/notfound.php
@@ -4,4 +4,4 @@ require_once __DIR__ . '/../vendor/autoload.php';
 $loader = new Twig_Loader_Filesystem(__DIR__ . '/../templates');
 $twig = new Twig_Environment($loader);
 
-echo $twig->render('error.html.twig', array('bodyId' => 'notfound'));
+echo $twig->render('notfound.html.twig', array('bodyId' => 'notfound'));
