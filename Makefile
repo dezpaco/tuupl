37a7be1a658d0dd2c829096a51ea7ab796e6c5ba
Derry Lozano-Hoyland <derry@derryhoyland.com>
2014-08-07 11:32:05 +0100

    Added Gulp task to minify convert.js

--- Makefile
+++ Makefile
@@ -3,10 +3,13 @@
 npm:
 	npm install
 
+gulp-js:
+	gulp js
+
 gulp-sass:
 	gulp sass
 
-install: npm templates gulp-sass
+install: npm templates gulp-sass gulp-js
 
 templates:
 	test -f composer.phar || curl -sS https://getcomposer.org/installer | php
--- gulpfile.js
+++ gulpfile.js
@@ -1,26 +1,40 @@
 // Load plugins
 var gulp = require('gulp'),
     sass = require('gulp-ruby-sass'),
-    notify = require('gulp-notify');
+    notify = require('gulp-notify'),
+    uglify = require('gulp-uglify'),
+    rename = require("gulp-rename");
 
 // Set the default paths for reusability
 var paths = {
-  sass: 'public/assets/sass/*.scss',
-  css: 'public/assets/css'
+    convert: 'public/assets/js/convert.js',
+    js: 'public/assets/js',
+    sass: 'public/assets/sass/*.scss',
+    css: 'public/assets/css'
 };
 
 // Styles
 gulp.task('sass', function() {
-  return gulp.src(paths.sass)
-    .pipe(sass({ style: 'compressed' }))
-    .pipe(gulp.dest(paths.css))
-    .pipe(notify({ message: 'YKYM: <%= file.relative %>' }));
+    return gulp.src(paths.sass)
+        .pipe(sass({ style: 'compressed' }))
+        .pipe(gulp.dest(paths.css))
+        .pipe(notify({ message: 'YKYM: <%= file.relative %>' }));
+});
+
+// Scripts
+gulp.task('js', function() {
+    return gulp.src(paths.convert)
+        .pipe(rename('convert.min.js'))
+        .pipe(uglify())
+        .pipe(gulp.dest(paths.js))
+        .pipe(notify({ message: 'YKYM: <%= file.relative %>' }));
 });
 
 // Watch files for changes and do something
 gulp.task('watch', function() {
-  gulp.watch(paths.sass, ['sass']);
+    gulp.watch(paths.sass, ['sass']);
+    gulp.watch(paths.convert, ['js']);
 });
 
 // Default task. This is for when you just type 'gulp' on the command line
-gulp.task('default', ['sass']);
+gulp.task('default', ['sass', 'js']);
