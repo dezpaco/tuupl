c90c5e1ebb04c93de37011c3869292b6f66ae336
Derry Lozano-Hoyland <derry@derryhoyland.com>
2016-01-31 21:56:59 +0000

    Reworked styling with Autoprefixer

--- gulpfile.js
+++ gulpfile.js
@@ -1,33 +1,39 @@
 'use strict';
 
-// Load plugins
-var gulp = require('gulp'),
-    sass = require('gulp-sass'),
-    notify = require('gulp-notify'),
-    uglify = require('gulp-uglify'),
-    rename = require("gulp-rename");
+var gulp = require('gulp');
+var autoprefixer = require('gulp-autoprefixer');
+var sass = require('gulp-sass');
+var notify = require('gulp-notify');
+var uglify = require('gulp-uglify');
+var rename = require("gulp-rename");
+
+var autoprefixerOptions = {
+    browsers: ['last 2 versions'],
+    cascade: false
+};
 
 // Set the default paths for reusability
 var paths = {
     convert: 'public/assets/js/convert.js',
     js: 'public/assets/js',
-    sass: 'public/assets/sass/*.scss',
+    sass: 'public/assets/sass/core.scss',
     css: 'public/assets/css'
 };
 
 
 // Styles
 gulp.task('sass', function() {
-  gulp.src(paths.sass)
-    .pipe(sass({ outputStyle: 'compressed' }))
-    .pipe(gulp.dest(paths.css))
-    .pipe(sass.sync().on('error', sass.logError))
-    .pipe(notify({ message: 'YKYM: <%= file.relative %>' }));
+    gulp.src(paths.sass)
+        .pipe(sass({ outputStyle: 'compressed' }))
+        .pipe(autoprefixer(autoprefixerOptions))
+        .pipe(gulp.dest(paths.css))
+        .pipe(sass.sync().on('error', sass.logError))
+        .pipe(notify({ message: 'YKYM: <%= file.relative %>' }));
 });
 
 // Scripts
 gulp.task('js', function() {
-    return gulp.src(paths.convert)
+    gulp.src(paths.convert)
         .pipe(rename('convert.min.js'))
         .pipe(uglify())
         .pipe(gulp.dest(paths.js))
